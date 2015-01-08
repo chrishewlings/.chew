b9d5899f1c234f5c5134664df8aee51a9d07a4d5
chrishewlings <chris.hewlings@gmail.com>
2015-01-08 08:53:57 -0500

    Create dotfiles_install.sh
    installer for dotfiles repo

---
+++ bash/dotfiles_install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+directory="$HOME/dotfiles"
+backup_existing="$HOME/dotfiles_old"
+
+files="bashrc vimrc bash_profile bash_functions"
+
+mkdir -p $backup_existing
+
+cd $directory
+
+for file in $files; do
+		mv ~/.$file $backup_existing
+		ln -s $directory/$file ~/.$file
+done
