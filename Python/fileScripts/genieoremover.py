3388322a6bdd1508e5171d5139cb617039b06653
Chris Hewlings <chris.hewlings@gmail.com>
2015-01-13 09:51:34 -0500

    updated the malware remover with a couple more file defintions, expanding it to delete other things than just genieo

--- Python/fileScripts/genieoremover.py
+++ Python/fileScripts/genieoremover.py
@@ -1,9 +1,9 @@
-#! /usr/bin/env python
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
 import os, sys, errno, subprocess
 
-
+newFileList = []
 
 # definitions
 def removeElementsNicely(list):
@@ -18,8 +18,17 @@ def removeElementsNicely(list):
 				os.rmdir(element) #fail silently and attmept to remove element if it a directory
 				pass
 
-def displayWindow():
-	subprocess.call('osascript -e "display dialog')
+def unpackPaths(fileList):
+	# unpack paths correctly for current user
+
+	for value in fileList:
+		newFileList.append(os.path.expanduser(value))
+	return newFileList
+
+
+def allDoneByeBye():
+	raw_input("The script has finished executing.  Please close any important files and press any key to reboot the system. ")
+	subprocess.call(["shutdown","-r", "now"])
 	
 
 
@@ -30,40 +39,48 @@ if os.geteuid() != 0:
 	sys.exit(1)
 '''
 
-# define which files to remove
+# define which files to remove, can be appended later if needed
 
-userLibraryFiles = ['~/Library/Application Support/com.genieoinnovation.Installer/',
+genieoFiles = ['~/Library/Application Support/com.genieoinnovation.Installer/',
 '~/Library/Application Support/Genieo/',
 '~/Library/LaunchAgents/com.genieo.completer.download.plist',
-'~/Library/LaunchAgents/com.genieo.completer.update.plist']
-
-rootLibraryFiles = ['/Library/LaunchAgents/com.genieoinnovation.macextension.plist',
+'~/Library/LaunchAgents/com.genieo.completer.update.plist',
+'/Library/LaunchAgents/com.genieoinnovation.macextension.plist',
 '/Library/LaunchAgents/com.genieoinnovation.macextension.client.plist',
 '/Library/LaunchAgents/com.genieo.engine.plist',
 '/Library/LaunchAgents/com.genieo.completer.update.plist',
 '/Library/LaunchDaemons/com.genieoinnovation.macextension.client.plist',
 '/Library/PrivilegedHelperTools/com.genieoinnovation.macextension.client',
-'/Library/Frameworks/GenieoExtra.framework']
-
-usrlibFiles = ['/usr/lib/libgenkit.dylib',
+'/Library/Frameworks/GenieoExtra.framework',
+'/usr/lib/libgenkit.dylib',
 '/usr/lib/libgenkitsa.dylib',
 '/usr/lib/libimckit.dylib',
-'/usr/lib/libimckitsa.dylib']
-
-applicationFiles = ['/Applications/Genieo',
+'/usr/lib/libimckitsa.dylib',
+'/Applications/Genieo',
 '/Applications/Uninstall Genieo',
-'/Applications/Uninstall IM Completer.app']
+'/Applications/Uninstall IM Completer.app',
+'/Application/InstallMac']
 
-# unpack paths correctly for current user
+vsearchFiles =  ['/Library/LaunchAgents/com.vsearch.agent.plist',
+'/Library/LaunchDaemons/com.vsearch.daemon.plist',
+'/Library/LaunchDaemons/com.vsearch.helper.plist',
+'/System/Library/Frameworks/VSearch.framework',
+'/Library/Application Support/VSearch']
 
-for index, value in enumerate(userLibraryFiles):
-	userLibraryFiles[index] = os.path.expanduser(userLibraryFiles[index])
+conduitFiles = ['/Library/InputManagers/CTLoader/',
+'/Library/LaunchAgents/com.conduit.loader.agent.plist',
+'/Library/LaunchDaemons/com.perion.searchprotectd.plist',
+'/Library/Application Support/SIMBL/Plugins/CT2285220.bundle',
+'/Library/Application Support/Conduit/',
+'/Applications/SearchProtect.app',
+'/Applications/SearchProtect/',
+'~/Library/Application Support/Conduit/',
+'~/Library/Internet Plug-Ins/ConduitNPAPIPlugin.plugin',
+'~/Library/Internet Plug-Ins/TroviNPAPIPlugin.plugin',
+'~/Conduit/',
+'~/Trovi/']
 
-			
-#removeElementsNicely(userLibraryFiles)
-#removeElementsNicely(rootLibraryFiles)
-#removeElementsNicely(usrlibFiles)
-#removeElementsNicely(applicationFiles)
+#spigotFiles = ['~/Library/Application Support/Spigot/']
+# need a wildcard for anything with slick savings, ebay shopping assistant, etc
 
-displayWindow()
 # prompt the user to restart the system
